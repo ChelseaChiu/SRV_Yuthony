68374c91b2b028929f18914d375c47f810b86821
ChelseaChiu <chelseachiu.happy@gmail.com>
2019-04-12 13:38:22 +0930

    Initail c# application with xaml pages

---
+++ SRV_UWP/SRV_UWP/viewmodels/DetailsViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRV_UWP.viewmodels
+{
+    public class DetailsViewModel
+    {
+    }
+}
